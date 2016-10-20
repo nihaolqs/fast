911d46c9a964f44cadc6b7018aca2aa9880b0f93
dell <nihaolqs@163.com>
2016-10-20 18:29:33 +0800

    详情界面

--- gamestore/src/main/java/com/lqs/fast/gamestore/fragment/ManagerFragment.java
+++ gamestore/src/main/java/com/lqs/fast/gamestore/fragment/ManagerFragment.java
@@ -1,15 +1,16 @@
 package com.lqs.fast.gamestore.fragment;
 
+import android.os.Handler;
 import android.view.View;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import com.lqs.fast.fast.base.presenter.ABasePresenter;
 import com.lqs.fast.fast.base_ui.ABaseFragment;
+import com.lqs.fast.fast.utils.FileUtil;
 import com.lqs.fast.fast.utils.MvpUtils;
 import com.lqs.fast.gamestore.R;
 import com.lqs.fast.gamestore.adatpter.MyLvGameManagerAdatpter;
-import com.lqs.fast.gamestore.bean.GameInfoBean;
 import com.lqs.fast.gamestore.bean.SaveGameInfoBean;
 import com.lqs.fast.gamestore.model.ManagerFragmentModel;
 import com.lqs.fast.gamestore.presenter.DownLoadPresenter;
@@ -27,7 +28,7 @@ import java.util.List;
  * Created by dell on 2016/10/14.
  */
 
-public class ManagerFragment extends ABaseFragment<ManagerFragment,String> implements IManagerView,IDownLoadView{
+public class ManagerFragment extends ABaseFragment<ManagerFragment, String> implements IManagerView, IDownLoadView {
     public static final String TAG = "ManagerFragment";
     private ListView mLvGameManager;
     private TextView mTvPhoneMemory;
@@ -35,11 +36,13 @@ public class ManagerFragment extends ABaseFragment<ManagerFragment,String> imple
     private TextView mTvAvailable;
 
     private ArrayList<SaveGameInfoBean> mGameInfoBeenList = new ArrayList<>();
+    private MyLvGameManagerAdatpter mMyLvGameManagerAdatpter;
+
     @Override
     protected void initMvp() {
         ManagerFragmentModel managerFragmentModel = new ManagerFragmentModel();
         ManagerFragmentPresenter managerFragmentPresenter = new ManagerFragmentPresenter(getContext());
-        MvpUtils.initMVP(managerFragmentModel,managerFragmentPresenter,this);
+        MvpUtils.initMVP(managerFragmentModel, managerFragmentPresenter, this);
     }
 
     @Override
@@ -49,7 +52,7 @@ public class ManagerFragment extends ABaseFragment<ManagerFragment,String> imple
     }
 
     private void initListView() {
-        MyLvGameManagerAdatpter myLvGameManagerAdatpter = new MyLvGameManagerAdatpter(mGameInfoBeenList,getContext(),getDownLoadPresenter());
+        mMyLvGameManagerAdatpter = new MyLvGameManagerAdatpter(mGameInfoBeenList, getContext(), getDownLoadPresenter());
         mLvGameManager.setAdapter(null);
     }
 
@@ -126,34 +129,135 @@ public class ManagerFragment extends ABaseFragment<ManagerFragment,String> imple
         getDownLoadPresenter().setDownLoadListener(new MyDownLoadService.IDownLoadListener() {
             @Override
             public void wail(String url) {
-
+                setItemDownloadStateWail(url);
             }
 
             @Override
-            public void progress(String url, int progre) {
-
+            public void progress(String url, final int progre) {
+                setItemDownloadState(url, progre+"%");
             }
 
             @Override
             public void completed(String url) {
-
+                setItemDownloadState(url, "完成");
             }
 
             @Override
             public void fail(String url) {
-
+                setItemDownloadState(url, "重试");
             }
 
             @Override
-            public void speed(String url, long speed) {
-
+            public void speed(String url, final long speed) {
+                setItemDownloadStateSpeed(url, speed);
             }
 
             @Override
-            public void downloadedSize(String url, long size) {
-
+            public void downloadedSize(String url, final long size) {
+                setItemDownloadStateSize(url, size);
             }
         });
+    }
+
+    private void setItemDownloadStateSpeed(String url, final long speed) {
+        Handler handler = new Handler();
+        for (int i = 0; i < mGameInfoBeenList.size(); i++) {
+            int itemViewType = mMyLvGameManagerAdatpter.getItemViewType(i);
+            final SaveGameInfoBean bean = mGameInfoBeenList.get(i);
+            if (bean.getDownload_url().equals(url) &&
+                    i >= mLvGameManager.getFirstVisiblePosition() &&
+                    i < mLvGameManager.getLastVisiblePosition() &&
+                    itemViewType == 1) {
+                View itemView = mLvGameManager.getChildAt(i - mLvGameManager.getFirstVisiblePosition());
+                final TextView mTvSpeed = (TextView) itemView.findViewById(R.id.item_download_tv_internetspeed);
+                final TextView mTvDownloadState = (TextView) itemView.findViewById(R.id.item_download_tv_download_state);
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        mTvDownloadState.setVisibility(View.GONE);
+                        mTvSpeed.setVisibility(View.VISIBLE);
+                        mTvDownloadState.setText(FileUtil.formatDownLoadSpeed(speed));
+                    }
+                });
+            }
+        }
+    }
+
+    private void setItemDownloadStateSize(String url, final long size) {
+        Handler handler = new Handler();
+        for (int i = 0; i < mGameInfoBeenList.size(); i++) {
+            int itemViewType = mMyLvGameManagerAdatpter.getItemViewType(i);
+            final SaveGameInfoBean bean = mGameInfoBeenList.get(i);
+            if (bean.getDownload_url().equals(url) &&
+                    i >= mLvGameManager.getFirstVisiblePosition() &&
+                    i < mLvGameManager.getLastVisiblePosition() &&
+                    itemViewType == 1) {
+                View itemView = mLvGameManager.getChildAt(i - mLvGameManager.getFirstVisiblePosition());
+                final TextView mTvDownloadding = (TextView) itemView.findViewById(R.id.item_download_tv_downloadding);
+                final TextView mTvInstallState = (TextView) itemView.findViewById(R.id.item_download_tv_install_state);
+                final String gamesize = mGameInfoBeenList.get(i).getGamesize();
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        mTvInstallState.setVisibility(View.GONE);
+                        mTvDownloadding.setVisibility(View.VISIBLE);
+                        mTvDownloadding.setText(FileUtil.formatSize(size) + "/" + gamesize);
+                    }
+                });
+            }
+        }
+    }
+
+    private void setItemDownloadState(String url, final String stateStr) {
+        Handler handler = new Handler();
+        for (int i = 0; i < mGameInfoBeenList.size(); i++) {
+            int itemViewType = mMyLvGameManagerAdatpter.getItemViewType(i);
+            final SaveGameInfoBean bean = mGameInfoBeenList.get(i);
+            if (bean.getDownload_url().equals(url) &&
+                    i >= mLvGameManager.getFirstVisiblePosition() &&
+                    i < mLvGameManager.getLastVisiblePosition() &&
+                    itemViewType == 1) {
+                View itemView = mLvGameManager.getChildAt(i - mLvGameManager.getFirstVisiblePosition());
+                final TextView tvState = (TextView) itemView.findViewById(R.id.item_download_tv_state);
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        tvState.setText(stateStr);
+                    }
+                });
+            }
+        }
+    }
+
+    private void setItemDownloadStateWail(String url) {
+        Handler handler = new Handler();
+        for (int i = 0; i < mGameInfoBeenList.size(); i++) {
+            int itemViewType = mMyLvGameManagerAdatpter.getItemViewType(i);
+            final SaveGameInfoBean bean = mGameInfoBeenList.get(i);
+            if (bean.getDownload_url().equals(url) &&
+                    i >= mLvGameManager.getFirstVisiblePosition() &&
+                    i < mLvGameManager.getLastVisiblePosition() &&
+                    itemViewType == 1) {
+                View itemView = mLvGameManager.getChildAt(i - mLvGameManager.getFirstVisiblePosition());
+                final TextView mTvSpeed = (TextView) itemView.findViewById(R.id.item_download_tv_internetspeed);
+                final TextView mTvDownloadState = (TextView) itemView.findViewById(R.id.item_download_tv_download_state);
+                final TextView mTvDownloadding = (TextView) itemView.findViewById(R.id.item_download_tv_downloadding);
+                final TextView mTvInstallState = (TextView) itemView.findViewById(R.id.item_download_tv_install_state);
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        mTvDownloadState.setVisibility(View.VISIBLE);
+                        mTvSpeed.setVisibility(View.GONE);
+                        mTvDownloadState.setText("等待");
+                        mTvDownloadding.setVisibility(View.VISIBLE);
+                        mTvInstallState.setVisibility(View.GONE);
+                        mTvDownloadding.setText("0M/" + bean.getGamesize() + "M");
+                    }
+                });
+
+
+            }
+        }
     }
 
     @Override
